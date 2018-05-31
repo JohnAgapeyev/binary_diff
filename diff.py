71dfc36bbe9edf2fe7006e08565257b7c98a2eb6
John Agapeyev <jagapeyev@gmail.com>
2018-05-31 14:25:30 -0700

    Added the ability to grab the n closest files

--- diff.py
+++ diff.py
@@ -1,37 +1,36 @@
 #!/bin/python3
 import sys
+import os
+from collections import defaultdict
 import tlsh
 
 if len(sys.argv) < 3:
     print("Requires a minimum of two files as arguments")
     exit(1)
 
-if len(open(sys.argv[1], 'rb').read()) < 256:
+if os.path.getsize(sys.argv[1]) < 256:
     print("Input file must be a minimum of 256 bytes\n")
     exit(1)
 
 base = tlsh.hash(open(sys.argv[1], 'rb').read())
-file_hashes = {}
-closest_files = []
-min_dist = (2**16) - 1
+n = 10
+test_col = defaultdict(list)
 
 for arg in sys.argv:
     if arg == sys.argv[0] or arg == sys.argv[1]:
         continue
 
-    if len(open(arg, 'rb').read()) < 256:
+    if os.path.getsize(arg) < 256:
         print("Test file must be a minimum of 256 bytes\n")
         exit(1)
 
     new_hash = tlsh.hash(open(arg, 'rb').read())
-    file_hashes[arg] = new_hash
     diff = tlsh.diff(base, new_hash)
-    if diff < min_dist:
-        min_dist = diff
-        closest_files = [arg]
-    elif diff == min_dist:
-        closest_files.insert(arg)
+    test_col[diff].append((arg, new_hash))
 
-print(closest_files)
 print("Base hash: " + str(base))
-print(file_hashes)
+
+for i in sorted(test_col)[:n]:
+    print("Distance " + str(i))
+    for elem in test_col[i]:
+        print(elem[0])
