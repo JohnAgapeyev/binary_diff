8d5be089c234ec810c0ccce005278cc30eff2a9c
John Agapeyev <jagapeyev@gmail.com>
2018-06-01 16:56:17 -0700

    Added different args parsing and limiting output based on n

--- diff.py
+++ diff.py
@@ -1,19 +1,44 @@
 #!/bin/python3
 import sys
 import os
+import getopt
 from collections import defaultdict
 import tlsh
 
-if len(sys.argv) < 3:
-    print("Requires a minimum of two arguments")
+def usage():
+    print("python3 ./diff.py [args] [in_file] [compare_files]")
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hdn:", ["help", "dump", "number"])
+except getopt.GetoptError as err:
+    print(err) # will print something like "option -a not recognized"
+    usage()
+    exit(2)
+
+dump = False
+n = 0
+file_inputs = args
+
+for o, a in opts:
+    if o in ("-n", "--number"):
+        n = int(a)
+    elif o in ("-h", "--help"):
+        usage()
+        exit()
+    elif o in ("-d", "--dump"):
+        dump = True
+
+if n <= 0:
+    print("N not set, defaulting to 10")
+    n = 10
+
+if len(file_inputs) < 2:
+    print("Program must contain at least 2 file inputs")
     exit(1)
 
-if sys.argv[1] == "-d":
+if dump:
     out_data = {}
-    for arg in sys.argv:
-        if arg == sys.argv[0] or arg == sys.argv[1]:
-            continue
-
+    for arg in file_inputs:
         if os.path.getsize(arg) < 256:
             print("Input file must be a minimum of 256 bytes\n")
             exit(1)
@@ -24,20 +49,16 @@ if sys.argv[1] == "-d":
     for elem in sorted(out_data):
         print('{},{}'.format(elem, out_data[elem]))
 
-    exit(0)
+    exit()
 
-if os.path.getsize(sys.argv[1]) < 256:
+if os.path.getsize(file_inputs[0]) < 256:
     print("Input file must be a minimum of 256 bytes\n")
     exit(1)
 
-base = tlsh.hash(open(sys.argv[1], 'rb').read())
-n = 10
+base = tlsh.hash(open(file_inputs[0], 'rb').read())
 test_col = defaultdict(list)
 
-for arg in sys.argv:
-    if arg == sys.argv[0] or arg == sys.argv[1]:
-        continue
-
+for arg in file_inputs[1:]:
     if os.path.getsize(arg) < 256:
         print("Test file must be a minimum of 256 bytes\n")
         exit(1)
@@ -48,8 +69,12 @@ for arg in sys.argv:
 
 print("Base hash: " + str(base))
 
+count = 0
 for i in sorted(test_col)[:n]:
-    print("Distance " + str(i))
+    print("\nDistance " + str(i))
     for elem in test_col[i]:
         print(elem[0])
         print(elem[1])
+        count += 1
+        if count >= n:
+            exit()
