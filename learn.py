61769bd67975e6d112eafea74a856b5c04a57793
John Agapeyev <jagapeyev@gmail.com>
2018-07-24 16:19:01 -0700

    Finally imported multiple files as tensors into dataset

--- learn.py
+++ learn.py
@@ -8,6 +8,7 @@ import collections
 import numpy as np
 import tensorflow as tf
 
+from PIL import Image
 from multiprocessing.dummy import Pool
 
 #file_data = np.fromfile(sys.argv[1], np.uint8)
@@ -22,37 +23,50 @@ from multiprocessing.dummy import Pool
 #
 #print(file_tensor)
 
-data = np.fromfile(sys.argv[1], np.uint8)
-file_width = math.ceil(math.sqrt(len(data)))
-data.resize((file_width, file_width))
-place_hold = tf.placeholder(data.dtype, data.shape)
+#data = np.fromfile(sys.argv[1], np.uint8)
+#file_width = math.ceil(math.sqrt(len(data)))
+#data.resize((file_width, file_width))
+#place_hold = tf.placeholder(data.dtype, data.shape)
+#
+##dataset = tf.data.Dataset.from_tensor_slices(place_hold)
+#dataset = tf.data.Dataset.from_tensors(place_hold)
+#iterator = dataset.make_initializable_iterator()
+#next_elem = iterator.get_next()
+#
+#
+#
+#
+#data2 = np.fromfile(sys.argv[2], np.uint8)
+#file_width2 = math.ceil(math.sqrt(len(data2)))
+#data2.resize((file_width2, file_width2))
+#place_hold2 = tf.placeholder(data2.dtype, data2.shape)
+#
+##dataset = tf.data.Dataset.from_tensor_slices(place_hold)
+#dataset2 = tf.data.Dataset.from_tensors(place_hold2)
+#iterator2 = dataset2.make_initializable_iterator()
+#next_elem2 = iterator2.get_next()
+#
+#with tf.Session() as sess:
+    #sess.run(iterator.initializer, feed_dict = {place_hold: data})
+    #sess.run(iterator.initializer, feed_dict = {place_hold2: data2})
+#
+    #dataset = dataset.concatenate(dataset2)
+#
+    #t_data = sess.run(next_elem)
+    #print(t_data)
+#
 
-#dataset = tf.data.Dataset.from_tensor_slices(place_hold)
-dataset = tf.data.Dataset.from_tensors(place_hold)
-iterator = dataset.make_initializable_iterator()
-next_elem = iterator.get_next()
+tf.enable_eager_execution()
 
+ten = []
 
+for arg in sys.argv:
+    data = np.fromfile(arg, np.uint8)
+    file_width = math.ceil(math.sqrt(len(data)))
+    data.resize((file_width, file_width))
+    ten.append(tf.convert_to_tensor(data))
 
-
-data2 = np.fromfile(sys.argv[2], np.uint8)
-file_width2 = math.ceil(math.sqrt(len(data2)))
-data2.resize((file_width2, file_width2))
-place_hold2 = tf.placeholder(data2.dtype, data2.shape)
-
-#dataset = tf.data.Dataset.from_tensor_slices(place_hold)
-dataset2 = tf.data.Dataset.from_tensors(place_hold2)
-iterator2 = dataset2.make_initializable_iterator()
-next_elem2 = iterator2.get_next()
-
-with tf.Session() as sess:
-    sess.run(iterator.initializer, feed_dict = {place_hold: data})
-    sess.run(iterator.initializer, feed_dict = {place_hold2: data2})
-
-    dataset = dataset.concatenate(dataset2)
-
-    t_data = sess.run(next_elem)
-    print(t_data)
-
-
-
+print(ten)
+#dataset = tf.data.Dataset.from_tensors(ten)
+#for e in dataset.make_one_shot_iterator():
+    #print(e)
