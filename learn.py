2f12ffefafc38ff0572515b5a7af7916968ec3db
John Agapeyev <jagapeyev@gmail.com>
2018-07-25 10:05:44 -0700

    Removed unused code, and added initial model function

--- learn.py
+++ learn.py
@@ -11,50 +11,54 @@ import tensorflow as tf
 from PIL import Image
 from multiprocessing.dummy import Pool
 
-#file_data = np.fromfile(sys.argv[1], np.uint8)
-#
-#print(len(file_data))
-#
-#file_width = math.ceil(math.sqrt(len(file_data)))
-#
-#file_data.resize((file_width, file_width))
-#
-#file_tensor = tf.convert_to_tensor(file_data)
-#
-#print(file_tensor)
+def cnn_model_fn(features, labels, mode):
+    input_layer = tf.reshape(features["x"], [-1, 1024, 1024, 1])
 
-#data = np.fromfile(sys.argv[1], np.uint8)
-#file_width = math.ceil(math.sqrt(len(data)))
-#data.resize((file_width, file_width))
-#place_hold = tf.placeholder(data.dtype, data.shape)
-#
-##dataset = tf.data.Dataset.from_tensor_slices(place_hold)
-#dataset = tf.data.Dataset.from_tensors(place_hold)
-#iterator = dataset.make_initializable_iterator()
-#next_elem = iterator.get_next()
-#
-#
-#
-#
-#data2 = np.fromfile(sys.argv[2], np.uint8)
-#file_width2 = math.ceil(math.sqrt(len(data2)))
-#data2.resize((file_width2, file_width2))
-#place_hold2 = tf.placeholder(data2.dtype, data2.shape)
-#
-##dataset = tf.data.Dataset.from_tensor_slices(place_hold)
-#dataset2 = tf.data.Dataset.from_tensors(place_hold2)
-#iterator2 = dataset2.make_initializable_iterator()
-#next_elem2 = iterator2.get_next()
-#
-#with tf.Session() as sess:
-    #sess.run(iterator.initializer, feed_dict = {place_hold: data})
-    #sess.run(iterator.initializer, feed_dict = {place_hold2: data2})
-#
-    #dataset = dataset.concatenate(dataset2)
-#
-    #t_data = sess.run(next_elem)
-    #print(t_data)
-#
+    conv1 = tf.layers.conv2d(
+        inputs=input_layer,
+        filters=32,
+        kernel_size=[5,5],
+        padding='same',
+        activation=tf.nn.relu
+    )
+
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2,2], strides=2)
+
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=64,
+        kernel_size=[5,5],
+        padding='same',
+        activation=tf.nn.relu
+    )
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2,2], strides=2)
+
+    pool2_flat = tf.reshape(pool2, [-1, 7*7*64])
+    dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+    dropout = tf.layers.dropout(inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+
+    logits = tf.layers.dense(inputs=dropout, units=10)
+
+    predictions = {
+        "classes": tf.argmax(inputs=logits, axis=1),
+        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+    }
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+    eval_metric_ops = {
+        "accuracy": tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])
+    }
+    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
 
 tf.enable_eager_execution()
 
@@ -72,3 +76,4 @@ for arg in sys.argv:
 dataset = tf.data.Dataset.from_tensors(ten)
 for e in dataset.make_one_shot_iterator():
     print(e)
+
