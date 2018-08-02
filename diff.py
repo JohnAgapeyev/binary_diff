4e8a1eb9540af134375f171e4a5a8781d042043d
John Agapeyev <jagapeyev@gmail.com>
2018-08-02 15:36:50 -0700

    Added the ability to save and reuse clustering data

--- diff.py
+++ diff.py
@@ -21,11 +21,18 @@ from sklearn.cluster import *
 from sklearn import metrics
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.preprocessing import StandardScaler
+from sklearn.externals import joblib
 
 pool = Pool()
 
 def usage():
     print("python3 ./diff.py [file directory] [metadata file]")
+
+def from_matrix_to_vector(i, j, N):
+    if i <= j:
+        return i * N - (i - 1) * i / 2 + j - i
+    else:
+        return j * N - (j - 1) * j / 2 + i - j
 
 def partition_hashes(hash_list, file_list):
     output = {}
@@ -66,67 +73,6 @@ def convert_dist_to_confidence(d):
         return 20.7
     else:
         return 0
-
-def lsh(data):
-    filename = data[0]
-    if not data[1] or data[1] == None:
-        meta = []
-    else:
-        meta = [d for d in data[1] if d[-1] == os.path.basename(filename)][0]
-
-    if os.path.getsize(filename) < 256:
-        raise ValueError("{} must be at least 256 bytes".format(filename))
-
-    print(filename)
-
-    if tarfile.is_tarfile(filename):
-        with tarfile.open(filename, 'r') as tar:
-            for member in tar.getmembers():
-                if not member or member.size < 256:
-                    continue
-                try:
-                    meta.append(tlsh.hash(tar.extractfile(member).read()))
-                    if use_binwalk:
-                        for module in binwalk.scan(tar.extractfile(member).read(), signature=True, quiet=True):
-                            for result in module.results:
-                                meta.append(str(result.file.path))
-                                meta.append(str(result.offset))
-                                meta.append(str(result.description))
-                except:
-                    continue
-    elif zipfile.is_zipfile(filename):
-        try:
-            with zipfile.ZipFile(filename) as z:
-                for member in z.infolist():
-                    if not member or member.file_size < 256:
-                        continue
-                    try:
-                        with z.read(member) as zipdata:
-                            meta.append(tlsh.hash(zipdata))
-                            if use_binwalk:
-                                for module in binwalk.scan(zipdata):
-                                    for result in module.results:
-                                        meta.append(str(result.file.path))
-                                        meta.append(str(result.offset))
-                                        meta.append(str(result.description))
-                    except:
-                        continue
-        except:
-            pass
-
-    if use_binwalk:
-        for module in binwalk.scan(filename, signature=True, quiet=True):
-            for result in module.results:
-                meta.append(str(result.file.path))
-                meta.append(str(result.offset))
-                meta.append(str(result.description))
-
-    file_hash = tlsh.hash(open(filename, 'rb').read())
-
-    if not meta:
-        return file_hash
-    else:
-        return tlsh.hash(str.encode(file_hash + ''.join(map(str, meta))))
 
 def lsh_json(data):
     filename = data[0]
@@ -293,18 +239,8 @@ def get_n_closest_partial(n, hash_partition, hash_list):
         closest[filename] = smallest_files
     return closest
 
-#hl = []
-#for i in range(1000001):
-    #hl.append(tlsh.hash(os.urandom(256)))
-
-#part = partition_hashes(hl, [])
-
-#get_n_closest_partial(10, part, hl)
-
-#exit()
-
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "hd:m:bn:", ["help", "directory", "metadata", "binwalk", "number"])
+    opts, args = getopt.getopt(sys.argv[1:], "hd:m:bn:t", ["help", "directory", "metadata", "binwalk", "number", "test"])
 except getopt.GetoptError as err:
     print(err) # will print something like "option -a not recognized"
     usage()
@@ -314,6 +250,7 @@ directory = ""
 meta = ""
 use_binwalk = False
 n = 10
+use_existing = False
 
 for o, a in opts:
     if o in ("-d", "--directory"):
@@ -327,6 +264,8 @@ for o, a in opts:
         use_binwalk = True
     elif o in ("-n", "--number"):
         n = int(a)
+    elif o in ("-t", "--test"):
+        use_existing = True
 
 if not directory:
     print("Program must be provided a file directory path")
@@ -336,78 +275,100 @@ file_list = list_files(directory)
 hash_list = []
 
 if meta:
-    #meta_contents = parse_metadata(meta)
     meta_contents = parse_metadata_json(meta)
 else:
     meta_contents = None
 
 hash_list = [lsh_json(x) for x in zip(file_list, itertools.repeat(meta_contents))]
 
-#for key, value in get_n_closest_partial(n, partition_hashes(hash_list, file_list), hash_list).items():
-    #print(key)
-    #for dist, name in value:
-        #print(convert_dist_to_confidence(dist), dist, name)
+if use_existing:
+    file_data = np.load(".tmp.npz")
+    #See https://stackoverflow.com/questions/22315595/saving-dictionary-of-header-information-using-numpy-savez for why this syntax is needed
+    clustered_files = file_data['clusters'][()]
+    cluster_hashes = file_data['hash_list']
 
-#exit()
+    ms = joblib.load('.tmp2.pkl')
 
-adj = np.zeros((len(hash_list), len(hash_list)), int)
+    adj = np.zeros((len(hash_list), len(cluster_hashes)), int)
 
-for i in range(len(hash_list)):
-    for j in range(len(hash_list)):
-        d = diff_hash(hash_list[i], hash_list[j]);
-        adj[i][j] = d
-        adj[j][i] = d
+    #Compare new file hashes against saved data to get distances
+    for i in range(len(hash_list)):
+        for j in range(len(cluster_hashes)):
+            adj[i][j] = diff_hash(hash_list[i], cluster_hashes[j]);
 
-best_cluster_count = 0
-best_silhouette_score = -1.0
+    cluster_labels = ms.predict(adj)
 
-def cl(data):
-    i, adj = data
-    print("Trying cluster count {}".format(i))
-    return metrics.silhouette_score(adj, MiniBatchKMeans(n_clusters=i).fit_predict(adj))
+    for f in file_list:
+        #Label of the prediucted file cluster
+        lab = cluster_labels[file_list.index(f)]
+
+        if lab not in clustered_files:
+            print("{} does not belong to any existing cluster".format(f))
+            continue
+        clus = clustered_files[lab]
+        print("Target file {} is in cluster {}".format(f, lab))
+        for c in clus:
+            print(c)
+        #Empty line to separate cluster print outs
+        print()
+
+    exit()
+else:
+    adj = np.zeros((len(hash_list), len(hash_list)), int)
+
+    for i in range(len(hash_list)):
+        for j in range(len(hash_list)):
+            d = diff_hash(hash_list[i], hash_list[j]);
+            adj[i][j] = d
+            adj[j][i] = d
+
+    best_cluster_count = 0
+    best_silhouette_score = -1.0
+
+    def cl(data):
+        i, adj = data
+        print("Trying cluster count {}".format(i))
+        return metrics.silhouette_score(adj, MiniBatchKMeans(n_clusters=i).fit_predict(adj))
 
 #Calculate the best cluster count in parallel
-silhouette_list = Pool().map(cl, zip(range(2, 16), itertools.repeat(adj)))
-best_cluster_count = silhouette_list.index(max(silhouette_list)) + 2
+    silhouette_list = Pool().map(cl, zip(range(2, 16), itertools.repeat(adj)))
+    best_cluster_count = silhouette_list.index(max(silhouette_list)) + 2
 
-ms = MiniBatchKMeans(n_clusters=best_cluster_count)
+    ms = MiniBatchKMeans(n_clusters=best_cluster_count)
 
-cluster_labels = ms.fit_predict(adj)
+    cluster_labels = ms.fit_predict(adj)
 
-clustered_files = {}
-for f in file_list:
-    lab = cluster_labels[file_list.index(f)]
-    if lab in clustered_files:
-        clustered_files[lab].append(f)
-    else:
-        clustered_files[lab] = [f]
+    clustered_files = {}
+    for f in file_list:
+        lab = cluster_labels[file_list.index(f)]
+        if lab in clustered_files:
+            clustered_files[lab].append(f)
+        else:
+            clustered_files[lab] = [f]
 
-print(clustered_files)
+    print(clustered_files)
 
-labels = ms.labels_
-cluster_centers = ms.cluster_centers_
+    np.savez(".tmp", clusters=clustered_files, hash_list=hash_list)
+    joblib.dump(ms, '.tmp2.pkl')
 
-labels_unique = np.unique(labels)
-n_clusters_ = len(labels_unique)
+    labels = ms.labels_
+    cluster_centers = ms.cluster_centers_
 
-print("number of estimated clusters : %d" % n_clusters_)
+    labels_unique = np.unique(labels)
+    n_clusters_ = len(labels_unique)
 
-plt.figure(1)
-plt.clf()
+    print("number of estimated clusters : %d" % n_clusters_)
 
-colors = itertools.cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
-for k, col in zip(range(n_clusters_), colors):
-    my_members = labels == k
-    cluster_center = cluster_centers[k]
-    plt.plot(adj[my_members, 0], adj[my_members, 1], col + '.')
-    plt.plot(cluster_center[0], cluster_center[1], '+', markerfacecolor=col,
-             markeredgecolor='k', markersize=5)
-plt.title('Estimated number of clusters: %d' % n_clusters_)
-plt.show()
+    plt.figure(1)
+    plt.clf()
 
-#c = get_n_closest(n, file_list, adj)
-#for key, value in c.items():
-    #print(key)
-    #for dist, name in value:
-        #print(convert_dist_to_confidence(dist), dist, name)
+    colors = itertools.cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
+    for k, col in zip(range(n_clusters_), colors):
+        my_members = labels == k
+        cluster_center = cluster_centers[k]
+        plt.plot(adj[my_members, 0], adj[my_members, 1], col + '.')
+        plt.plot(cluster_center[0], cluster_center[1], '+', markerfacecolor=col,
+                 markeredgecolor='k', markersize=5)
+    plt.title('Estimated number of clusters: %d' % n_clusters_)
+    plt.show()
 
